dd2ffe722cfed76da0b4d7294a6f8d08518f85e4
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-04-04 21:40:57 +0300

    Adjust "Featured products" section

--- src/components/shared/product/featured-product-section.tsx
+++ src/components/shared/product/featured-product-section.tsx
@@ -5,11 +5,13 @@ function FeaturedProductSection({children}: React.HTMLAttributes<HTMLElement>) {
     return (
         <section className='section'>
             <div className="container">
-                <div className="section-heading">
-                    <h2 className='heading-2'>{site.home.featured_products.title}</h2>
-                </div>
-                <div className="section-inner">
-                    {children}
+                <div className='section-inner'>
+                    <div className="section-heading">
+                        <h2 className='heading-2'>{site.home.featured_products.title}</h2>
+                    </div>
+                    <div className="section-body">
+                        {children}
+                    </div>
                 </div>
             </div>
         </section>
