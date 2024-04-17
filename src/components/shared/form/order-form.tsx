1dd89688e4db67228cce5650e8b68cd791983069
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-04-17 15:26:32 +0300

    [pages/service] Adjust order form submit button

--- src/components/shared/form/order-form.tsx
+++ src/components/shared/form/order-form.tsx
@@ -78,7 +78,7 @@ export default function OrderForm({ className, variant }: OrderFormProps) {
                         </>
                     )}
                 />
-                <Button variant={variant || "outline"} type="submit" className="flex-1 shrink-0 w-fit text-base font-semibold border-primary-foreground border-2 text-primary-foreground">ОТПРАВИТЬ</Button>
+                <Button variant={variant || "outline"} type="submit" className="w-full md:w-fit text-base font-semibold border-primary-foreground border-2 text-primary-foreground">ОТПРАВИТЬ</Button>
             </form>
         </Form>
     )
