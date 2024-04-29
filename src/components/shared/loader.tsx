05f5af45e17ab256723e478984e497fa4d268a93
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-04-29 15:56:26 +0300

    Update Loader

--- src/components/shared/loader.tsx
+++ src/components/shared/loader.tsx
@@ -1,10 +1,10 @@
-import { Loader2 } from 'lucide-react'
+import { LoaderIcon } from 'lucide-react'
 import React from 'react'
 
 const Loader = () => {
   return (
     <div className="absolute grid place-items-center top-0 left-0 right-0 bottom-0 bg-muted/70">
-      <Loader2 className="h-8 w-8 animate-spin" />
+      <LoaderIcon className="h-8 w-8 animate-spin" />
     </div>
   )
 }
