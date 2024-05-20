da83a008b79907208d9fe1a77f02df74cd59271a
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-05-20 13:34:37 +0300

    fetch blog posts from WP

--- src/lib/api/service.ts
+++ src/lib/api/service.ts
@@ -34,3 +34,37 @@ export async function getPosts(first = 10) {
 
   return data?.posts?.nodes;
 }
+
+export async function getPostsByCategory(category = '') {
+  const data = await fetchAPI(
+    `query getPosts {
+    categories(where: {name: $category}) {
+        nodes {
+            name
+            posts {
+                nodes {
+                    featuredImage {
+                    node {
+                        sourceUrl
+                    }
+                    }
+                    slug
+                    title
+                    excerpt
+                    date
+                    content
+                }
+            }
+    }
+    }
+  }
+  `,
+    {
+      variables: {
+        category,
+      },
+    }
+  );
+
+  return data?.categories?.nodes;
+}
