efd4db38efc04bec3a7cce9f4e36cba70480c683
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-05-22 20:36:43 +0300

    Add get post by slug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,58 +2,55 @@ import { BreadcrumbBase } from '@/components/shared/breadcrumb';
 import PostDate from '@/components/shared/post/post-date';
 import { SectionBase, SectionBody, SectionHeading, SectionInner } from '@/components/shared/section';
 import { blog } from '@/content'
+import { getPostBySlug, getPosts } from '@/lib/api/service';
 import Image from 'next/image';
 
 type Props = {
 	params: { slug: string }
 }
 
-function getPost({ params }: Props) {
-	const post = blog.list.filter((item) => {
-		return item.slug === params.slug;
-	})[0];
+// export async function generateMetadata({ params }: Props) {
+// 	const post = getPostBySlug(params?.slug as string)
 
-	return post;
-}
-
-export async function generateMetadata({ params }: Props) {
-	const post = getPost({ params })
-
-	return {
-		title: post.title,
-		description: post.description,
-	}
-}
+// 	return {
+// 		title: post.title,
+// 		description: post.description,
+// 	}
+// }
 
 export async function generateStaticParams() {
-	const posts = blog.list;
+	const posts = await getPosts();
 
-	return posts.map((post) => ({
+	return posts.map((post: any) => ({
 		slug: post?.slug,
 	}))
 }
 
-export default function BlogPostPage({ params }: Props) {
-	const post = getPost({ params })
+export default async function BlogPostPage({ params }: Props) {
+	const post = await getPostBySlug(params?.slug as string);
+	const author = post?.author?.node?.name;
 
 	return (
 		<>
 			<SectionBase className='after:bg-foreground/40 after:absolute after:top-0 after:left-0 after:w-full after:h-full relative'>
-				<Image
-					width={1400}
-					height={900}
-					src={post.thumbnail}
-					alt={post.title}
-					className='absolute object-cover object-center top-0 left-0 w-full h-full'
-					priority
-				/>
+				{
+					post.featuredImage?.node?.sourceUrl &&
+					<Image
+						width={1400}
+						height={900}
+						src={post.featuredImage.node.sourceUrl}
+						alt={post.title}
+						className='absolute object-cover object-center top-0 left-0 w-full h-full'
+						priority
+					/>
+				}
 				<SectionInner className='relative z-10 h-screen grid place-content-center'>
 					<SectionHeading className='pb-5 text-primary-foreground text-center'>
-						<p className="paragraph capitalize font-bold">статья от {post.author}</p>
+						{author && <p className="paragraph capitalize font-bold">статья от {author}</p>}
 						<h1 className="heading-1">
 							{post.title}
 						</h1>
-						<p className="paragraph md:text-2xl">{post.description}</p>
+						{post.excerpt && <div className="paragraph md:text-2xl space-y-4" dangerouslySetInnerHTML={{ __html: post.excerpt }} />}
 					</SectionHeading>
 				</SectionInner>
 			</SectionBase>
@@ -63,8 +60,16 @@ export default function BlogPostPage({ params }: Props) {
 				</SectionHeading>
 				<SectionInner>
 					<div className='flex flex-wrap items-center gap-3 text-foreground/70 text-sm'>
-						<PostDate date={post.date} />
-						<span>{post.category}</span>
+						{post?.date && <PostDate date={post.date} />}
+						<div>
+							{
+								Array.from(new Set(post.categories?.nodes)).map((item: any) => {
+									return (
+										item?.name
+									)
+								}).join(', ')
+							}
+						</div>
 					</div>
 					<SectionBody>
 						<article className='prose mx-auto' dangerouslySetInnerHTML={{ __html: post.content }} />
